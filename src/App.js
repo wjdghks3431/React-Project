d50c4667ca44e5a2fa173615d5a66c7e132ab5a9
wjdghks3431 <wjdghks3431@naver.com>
2023-02-28 00:43:29 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    // jsx를 사용
+    <div className="gray-background"> 
+      <img src={logo} lat="logo"/>
+      <h2>Let's develop management system!</h2>
+    </div>
+  );
+}
+
+export default App;
